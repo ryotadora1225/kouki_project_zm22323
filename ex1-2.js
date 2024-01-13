3bce0ee03c988685f51cb5a1c9ec8590a7d47158
ryotadora1225 <131091974+ryotadora1225@users.noreply.github.com>
2024-01-13 23:04:56 +0900

    Update ex1-2.js

--- ex1-2.js
+++ ex1-2.js
@@ -1,2 +1,2 @@
 function imgchange1(){document.getElementById('logo2').src='./image/husigidane01.jpg';setTimeout("imgchange2()",3000);}
-function imgchange2(){document.getElementById('logo2').src='./image/img_01.png';}
+function imgchange2(){document.getElementById('logo2').src='./image/husigidane02.png';}
